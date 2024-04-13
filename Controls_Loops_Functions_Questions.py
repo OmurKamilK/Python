212885e77ba7f45235d0051d9fee9448b0160529
OmurKamilK <omurkygsz@hotmail.com>
2024-04-13 14:05:57 +0300

    Last update for Loops

--- Controls_Loops_Functions_Questions.py
+++ Controls_Loops_Functions_Questions.py
@@ -105,8 +105,67 @@ for Hak in range(3,0,-1):
         time.sleep(1)
         print("Cikis yapildi.")
         break
-      
-
 time.sleep(1)
 print("3 kere yanlis girdiniz,Cikis yapildi.")'''
+#7-)7- Kullanicidan bir sayi girilmesi istinilsin ekran girilen sayiya kadar olan sayilar yazdirilsin.
+'''Say=int(input("Bir sayi giriniz:\n"))
+for i in range(Say,0,-1):
+    i=i-1
+    print(i)'''
+#8-)Kullanicidan alt limit ust limit ve artis miktari istenilsin.Ekran alt limitten baslayarak artis
+# miktari kadar artirarak ust limite kadar sayilar ekrana yazdirilsin.
+'''Alt_limit=int(input("Alt limit giriniz"))
+Ust_limit=int(input("Ust limit giriniz"))
+Artis=int(input("Artis miktari giriniz"))
+for Alt_limit in range(Alt_limit,Ust_limit+1,Artis):
+    print(Alt_limit)'''
+#9-)Kullanicidan alt limit ve ust limit alinsin ve aradaki sayilarin toplami hesaplansin.
+'''Alt_limit=int(input("Alt limit giriniz"))
+Ust_limit=int(input("Ust limit giriniz"))
+Artis=int(input("Artis miktari giriniz"))
+sonuc=0
+for Alt_limit in range(Alt_limit,Ust_limit+1,Artis):
+    if(Alt_limit<=Ust_limit):
+        sonuc+=Alt_limit
+print(sonuc)'''
+#10-)Girilen sayinin asal olup olmadigini kontrol edip asalsa "Bu bir asal sayidir" degilse "Asal sayi degildir" seklinde
+# ekrana mesaj yaziniz.
+'''Sayi=int(input("Bir sayi giriniz:\n"))
+if(Sayi%2==0):
+    print("Asal sayidir")
+else:
+    print("Asal sayi degildir")'''
+#11-)-Girilen metnin tersten ve duzden birbirine esit olup olmadigini kontrol eden kodu yaziniz.
+# Ornek:
+# abcba == polindromdur
+# deded == polindromdur
+# dead  != polindrom degildir
+'''Met=input("Bir metin giriniz:\n")
+if(Met==Met[::-1]):
+    print("Polindromdur")
+else:
+    print("Polindrom degildir.")'''
+#12-)Girilen sayinin faktoriyelini hesaplayan kodu yaziniz eger deger sifirdan kucukse hata mesaji verin
+'''Numb=int(input("bir sayi giriniz:\n"))
+b=1
+if(Numb>=0):
+    for Numb in range(Numb, 0, -1):
+        b *= Numb
+    print(b)
+else:
+    print("Hata")'''
+#13-)Girilen sayinin tum pozitif bolenlerini bulunuz.
+'''a=int(input("Bir sayi giriniz"))
+b=a
+while(a>0 and b>0):
+    if(a % b == 0):
+        print(f"{b},{a} bolenidir ")
+
+    else:
+        print(f"{b},{a} boleni degildir.")
+    b-=1'''
+
+
+
+
 
